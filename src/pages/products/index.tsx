4199580e9ab13bdf6b54a671c26e279e37569d64
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 14:38:29 +0330

    chore: add button and handle navigate it

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,11 +1,29 @@
-import { Container, Text } from '~src/components';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, Container, Text } from '~src/components';
 
 export const ProductsPage = () => {
+	const navigate = useNavigate();
+
+	const handleNavigate = (to: string) => {
+		navigate(to);
+	};
+
 	return (
 		<Container>
 			<Text padding="0 0 10px" size={30} fontWeight="bold">
 				Products Page
 			</Text>
+
+			<Button
+				color="success"
+				radius="rounded"
+				type="button"
+				onClick={() => handleNavigate('/')}
+				css={{ paddingLeft: '15px', paddingRight: '15px' }}
+			>
+				Go Back To Home
+			</Button>
 		</Container>
 	);
 };
