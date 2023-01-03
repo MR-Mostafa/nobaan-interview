00c4c52d4bf49588c4af833c6a11e16f1cd99fb4
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:11:33 +0330

    feat: create 404 page component

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '~src/components';
+
+export const NotFound = () => {
+	return (
+		<Container>
+			<Text padding="0 0 10px" size={150} fontWeight="bold">
+				404
+			</Text>
+		</Container>
+	);
+};
