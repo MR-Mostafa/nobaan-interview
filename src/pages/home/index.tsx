ee48dc738b1a96746103c63a4ed844710f81e498
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:08:41 +0330

    feat: create index page component

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '~src/components';
+
+export const HomePage = () => {
+	return (
+		<Container>
+			<Text padding="0 0 10px" size={30} fontWeight="bold">
+				Home Page
+			</Text>
+		</Container>
+	);
+};
