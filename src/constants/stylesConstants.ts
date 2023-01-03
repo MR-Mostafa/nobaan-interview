5d51ff8ee1b6ebeade4dc9c61f10df4d319e93ac
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:05:41 +0330

    feat: create style constant & global types

---
+++ src/constants/stylesConstants.ts
@@ -0,0 +1,13 @@
+export const globalColors = {
+	danger: '#dc3545',
+	primary: '#007bff',
+	warning: '#ffc107',
+	success: '#28a745',
+	dark: '#212529',
+};
+
+export const globalRadius = {
+	rounded: '4px',
+	circle: '50%',
+	pill: '50rem',
+};
