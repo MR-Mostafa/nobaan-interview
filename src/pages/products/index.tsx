d1be48d47152116ff50fdb201300f009361c73b1
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:09:15 +0330

    feat: create products page component

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '~src/components';
+
+export const ProductsPage = () => {
+	return (
+		<Container>
+			<Text padding="0 0 10px" size={30} fontWeight="bold">
+				Products Page
+			</Text>
+		</Container>
+	);
+};
