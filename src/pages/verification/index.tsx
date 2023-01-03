8327ee570656df0e6c7088575a5ff3599c8cb4b1
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:09:40 +0330

    feat: create verification page component

---
+++ src/pages/verification/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '~src/components';
+
+export const VerificationPage = () => {
+	return (
+		<Container>
+			<Text padding="0 0 10px" size={30} fontWeight="bold">
+				Verification Page
+			</Text>
+		</Container>
+	);
+};
