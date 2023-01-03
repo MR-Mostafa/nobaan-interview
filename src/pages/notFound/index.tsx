b93ee3064d3f0f9c16b8ecd2dd127039b41acfb6
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:43:43 +0330

    refactor: add css props and change size

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -3,7 +3,7 @@ import { Container, Text } from '~src/components';
 export const NotFound = () => {
 	return (
 		<Container>
-			<Text padding="0 0 10px" size={150} fontWeight="bold">
+			<Text padding="0 0 10px" size={200} fontWeight="bold" css={{ textAlign: 'center' }}>
 				404
 			</Text>
 		</Container>
