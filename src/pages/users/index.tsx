76aa09d456bd834c26a62654009900964aaedab6
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-03 13:09:32 +0330

    feat: create users page component

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '~src/components';
+
+export const UsersPage = () => {
+	return (
+		<Container>
+			<Text padding="0 0 10px" size={30} fontWeight="bold">
+				Users Page
+			</Text>
+		</Container>
+	);
+};
