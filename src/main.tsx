0d393338265b1fdf078ac397bc08e8e27532d6e7
Mostafa Rahmati <m.rahmati70@hotmail.com>
2023-01-05 04:20:03 +0330

    feat: install error-boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import '~assets/styles/global.scss';
+import { ErrorFallback } from '~src/sections';
 import { store } from '~src/store';
 
 import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<Provider store={store}>
+				<RouterProvider router={router} />
+			</Provider>
+		</ErrorBoundary>
 	</React.StrictMode>,
 );
